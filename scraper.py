be9dcb24601b61dbf7a75c0d3a6af7866ddae414
satyr <murky.satyr@gmail.com>
2016-03-02 08:45:26 +0900

    absolute link / content / date

--- scraper.py
+++ scraper.py
@@ -1,13 +1,20 @@
 import scraperwiki
 import lxml.html
 
+data = []
 html = scraperwiki.scrape("http://crea.bunshun.jp/list/comic-essay-information")
 root = lxml.html.fromstring(html)
 
-ancs = root.cssselect("#contents .layout-news .section-date a")
-data = []
+divs = root.cssselect("#contents .layout-news .section-date")
 
-for a in ancs:
-  data.append({"link": a.get("href"), "title": a.text_content()})
+for div in divs:
+  a = div.cssselect("a")[0]
+  data.append({
+    "link"    : "http://crea.bunshun.jp" + a.get("href"),
+    "title"   : a.text_content(),
+    "content" : div.cssselect("p")[0].text_content(),
+    "date"    : div.cssselect(".date")[0].text_content().replace(".", "-") + \
+                "T00:00:00Z",
+  })
 
 scraperwiki.sqlite.save(unique_keys=["link"], data=data)
